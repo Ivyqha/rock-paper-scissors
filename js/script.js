c7105b44398b2e3b397449f380901f76cc8adc03
Ivy Ha <123954589+Ivyqha@users.noreply.github.com>
2023-04-12 11:02:39 +0100

    getComputerChoice function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log("hello world!");
+function getComputerChoice() {
+
+   var option = ['Rock', 'Paper', "Scissors"]; 
+   var choice = option[Math.floor(Math.random()*option.length)];
+   return("The computer chose: " + choice);
+    
+}
+
+console.log(getComputerChoice());
